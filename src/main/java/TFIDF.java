f672228cfec0cb77f2eebb8fe083d35a5b916699
spartan-phucnguyen <phuc.nguyen@c0x12c.com>
2024-05-04 19:51:27 +0700

    tfidf

--- src/main/java/TFIDF.java
+++ src/main/java/TFIDF.java
@@ -1,9 +1,14 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -13,6 +18,7 @@ import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class TFIDF {
+    private static final String TOTAL_DOC = "TotalDoc";
     public static class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
         public void map(Object key, Text value, Context context)
                 throws IOException, InterruptedException {
@@ -28,7 +34,7 @@ public class TFIDF {
     }
 
     public static class IntSumReducer extends Reducer<Text, Text, Text, Text> {
-        static int totalDoc = 0;
+        static long totalDoc = 0;
         public void reduce(Text key, Iterable<Text> values, Context context)
                 throws IOException, InterruptedException {
             // total terms in a doc
@@ -56,13 +62,93 @@ public class TFIDF {
 
         @Override
         protected void cleanup(Context context) {
+            context.getCounter(TOTAL_DOC, TOTAL_DOC).increment(totalDoc);
+        }
+    }
+
+    public static class TokenizerMapperJ2 extends Mapper<Object, Text, Text, Text> {
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String[] parts = value.toString().split("\\s+");
+            String docId = parts[0];
+            String freq = parts[1];
+
+            context.write(
+                    new Text(key.toString()), // term as key
+                    new Text(docId + " " + freq)
+            );
+        }
+    }
+
+    public static class IntSumReducerJ2 extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
             Configuration config = context.getConfiguration();
-            config.set("totalDoc", Integer.toString(totalDoc));
+            int totalDoc = Integer.parseInt(config.get(TOTAL_DOC));
+
+            // total terms in a doc
+            int count = 0;
+            HashSet<String> aggKeys = new HashSet<>();
+            for (Text value : values) {
+                count++;
+                String[] parts = value.toString().split("\\s+");
+                //              termId              docId
+                aggKeys.add(key.toString() + " " + parts[0]);
+            }
+
+
+            int finalCount = count;
+            for (String aggKey : aggKeys) {
+                double idf = Math.log((double) totalDoc / finalCount);
+                context.write(
+                        new Text(aggKey),
+                        new Text(Double.toString(idf))
+                );
+            }
+        }
+    }
+
+    public static class FinalMapper extends Mapper<Object, Text, Text, FloatWritable> {
+        Map<String, Float> TF = null;
+        Map<String, Float> IDF = new HashMap<>();
+        @Override
+        protected void setup(Context context) throws IOException {
+            Configuration conf = context.getConfiguration();
+            FileSystem fs = FileSystem.get(conf);
+
+            TF  = FileReader(fs, conf.get("TFPath"));
+            IDF = FileReader(fs, conf.get("IDFPath"));
+        }
+
+        private Map<String, Float> FileReader(FileSystem fs, String tfPath) throws IOException {
+            Map<String, Float> map = new HashMap<>();
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(new Path(tfPath))))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split("\\s+");
+
+                    map.put(parts[0] + " " + parts[1], Float.parseFloat(parts[2]));
+                }
+            }
+
+            return map;
+        }
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String[] parts = value.toString().split("\\s+");
+
+            String aggKey = key.toString() + " " + parts[0];
+            float tfidf = TF.get(aggKey) * IDF.get(aggKey);
+
+            context.write(new Text(aggKey), new FloatWritable(tfidf));
         }
     }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+        conf.set("TFPath", args[1] + ".tf.tmp/part-r-00000");
+        conf.set("IDFPath", args[1] + ".idf.tmp/part-r-00000");
 
         Job job = Job.getInstance(conf, "tf");
         job.setJarByClass(TFIDF.class);
@@ -76,25 +162,45 @@ public class TFIDF {
         job.setOutputValueClass(Text.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1] + ".tmp"));
+        FileOutputFormat.setOutputPath(job, new Path(args[1] + ".tf.tmp"));
 
         job.waitForCompletion(true);
 
         // ------------------------
+        long totalDoc = job.getCounters().findCounter(TOTAL_DOC, TOTAL_DOC).getValue();
+
+        conf.set(TOTAL_DOC, Long.toString(totalDoc));
+
         Job job2 = Job.getInstance(conf, "tdf");
         job2.setJarByClass(TFIDF.class);
 
-        job2.setMapperClass(TokenizerMapper.class);
-        job2.setReducerClass(IntSumReducer.class);
+        job2.setMapperClass(TokenizerMapperJ2.class);
+        job2.setReducerClass(IntSumReducerJ2.class);
 
         job2.setOutputKeyClass(Text.class);
         job2.setOutputValueClass(Text.class);
 
         job2.setInputFormatClass(KeyValueTextInputFormat.class);
 
-        FileInputFormat.addInputPath(job2, new Path(args[1] + ".tmp"));
-        FileOutputFormat.setOutputPath(job2, new Path(args[1]));
+        FileInputFormat.addInputPath(job2, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job2, new Path(args[1] + ".idf.tmp"));
 
-        System.exit(job2.waitForCompletion(true) ? 0 : 1);
+        job2.waitForCompletion(true);
+        // ------------------------
+        Job job3 = Job.getInstance(conf, "tdf");
+        job3.setJarByClass(TFIDF.class);
+
+        job3.setMapperClass(FinalMapper.class);
+
+        job3.setOutputKeyClass(Text.class);
+        job3.setOutputValueClass(FloatWritable.class);
+
+        job3.setInputFormatClass(KeyValueTextInputFormat.class);
+
+        FileInputFormat.addInputPath(job3, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job3, new Path(args[1]));
+
+        System.exit(job3.waitForCompletion(true) ? 0 : 1);
+
     }
 }
